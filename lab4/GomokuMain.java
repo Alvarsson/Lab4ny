e368e875ef7be66352945e34d5067d6bf38f2ebc
Alvarsson <a-alvarsson@hotmail.com>
2018-02-14 13:24:39 +0100

    commitar lab4 filer

---
+++ lab4/GomokuMain.java
@@ -0,0 +1,21 @@
+package lab4;
+import lab4.client.*;
+
+public class GomokuMain {
+	public static void main(String[] args) {
+		//Finns bättre sätt?
+		String arg = args[0]; 
+		int portNumber = Integer.parseInt(arg);
+		if( arg == null || portNumber < 0) {//This should check if more arguments than one.
+			portNumber = 4000;
+		}
+		try {
+			GomokuClient client = new GomokuClient(portNumber);
+		}
+		GomokuGameState gameState = new GomokuGameState(client);
+		GomokuGUI gui = new GomokuGUI();
+		
+		
+		
+	}
+}
