82fdfca3a145e1479a2bca1b025b365efe9bbd13
Alvarsson <a-alvarsson@hotmail.com>
2018-02-15 18:01:43 +0100

    Börjar jobba på GomokuGUI

--- lab4/gui/GomokuGUI.java
+++ lab4/gui/GomokuGUI.java
@@ -1,10 +1,14 @@
 package lab4.gui;
 import java.util.Observable;
 import java.util.Observer;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import lab4.client.GomokuClient;
 import lab4.data.GameGrid;
 import lab4.data.GomokuGameState;
+
 
 /*
  * The GUI class
@@ -22,11 +26,34 @@ public class GomokuGUI implements Observer{
 	 * @param c   The client that is responsible for the communication
 	 */
 	public GomokuGUI(GomokuGameState g, GomokuClient c){
+		/*this.gamestate = g;
+		this.gamestate.addObserver(this);*/
+		
 		this.client = c;
 		this.gamestate = g;
 		client.addObserver(this);
 		gamestate.addObserver(this);
 		
+		JFrame frame = new JFrame("fishdix");
+		GamePanel gameGridPanel = new GamePanel();
+		JLabel messageLabel = new JLabel("make a move");
+		JButton connectButton = new JButton("Connect");
+		JButton newGameButton = new JButton("New Game");
+		JButton disconnectButton = new JButton("Disconnect");
+		JPanel pane = new JPanel();
+		
+		pane.add(gameGridPanel);
+		pane.add(messageLabel);
+		pane.add(connectButton);
+		pane.add(newGameButton);
+		pane.add(disconnectButton);
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane();
+		frame.pack();
+		frame.setLocation(100,100);
+		frame.setVisible(true);
 		
 	}
 	
