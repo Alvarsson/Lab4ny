cb7b3ef34690474714039f629f45ce21d6a89d08
Alvarsson <a-alvarsson@hotmail.com>
2018-02-15 16:59:00 +0100

    Större delen av GameGrid är färdig, måste dock fixa checkDiagonal längst ner för att kolla om INROW i rad gäller där

--- lab4/GomokuMain.java
+++ lab4/GomokuMain.java
@@ -3,18 +3,18 @@ import lab4.client.*;
 import lab4.data.*;
 import lab4.gui.*;
 
+
 public class GomokuMain {
 	public static void main(String[] args) {
-		//Finns bättre sätt?
-		String arg = args[0]; 
-		int portNumber = Integer.parseInt(arg);
-		if( arg == null || portNumber < 0) {//This should check if more arguments than one.
+		
+		int portNumber = Integer.parseInt(args[0]);
+		if( args.length != 1 || portNumber < 0) {//This should check if more arguments than one.
 			portNumber = 4000;
 		}
 		try {
 			GomokuClient client = new GomokuClient(portNumber);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 		GomokuGameState gameState = new GomokuGameState(client);
 		GomokuGUI gui = new GomokuGUI();
--- lab4/data/GameGrid.java
+++ lab4/data/GameGrid.java
@@ -1,21 +1,42 @@
 package lab4.data;
-
+import javafx.util.*;
 import java.util.Observable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Represents the 2-d game grid
  */
 
 public class GameGrid extends Observable{
-
+	public static final int ME = 2;
+	public static final int OTHER = 1; 
+	public static final int EMPTY = 0;
+	private int size;
+	public int [][] multiarray;
+	public static final int INROW = 5;
 	
 	/**
 	 * Constructor
 	 * 
 	 * @param size The width/height of the game grid
 	 */
-	public GameGrid(int size){}
 	
+	public GameGrid(int size){
+		this.size = size;
+		multiarray = new int[size][size];
+		startArray();
+	}
+	
+	private void startArray() {
+		for(int x = 0; x < multiarray.length; x++) {
+			for(int y = 0; y < multiarray.length; y++) {
+				multiarray[x][y] = EMPTY;
+			}
+		}
+	}
+		
+		
 	/**
 	 * Reads a location of the grid
 	 * 
@@ -23,14 +44,18 @@ public class GameGrid extends Observable{
 	 * @param y The y coordinate
 	 * @return the value of the specified location
 	 */
-	public int getLocation(int x, int y){}
-	
+	public int getLocation(int x, int y){
+		return multiarray[x][y];
+	}
+		
 	/**
 	 * Returns the size of the grid
 	 * 
 	 * @return the grid size
 	 */
-	public int getSize(){}
+	public int getSize(){
+		return size;
+	}
 	
 	/**
 	 * Enters a move in the game grid
@@ -40,12 +65,27 @@ public class GameGrid extends Observable{
 	 * @param player
 	 * @return true if the insertion worked, false otherwise
 	 */
-	public boolean move(int x, int y, int player){}
-	
+	public boolean move(int x, int y, int player){
+		if(multiarray[x][y] == EMPTY) {
+			multiarray[x][y] = player;
+			setChanged();
+			notifyObservers();
+			return true;
+			
+		} else {
+			return false;
+		}
+	}
+		
 	/**
 	 * Clears the grid of pieces
 	 */
-	public void clearGrid(){}
+	public void clearGrid(){
+		startArray();
+		setChanged();
+		notifyObservers();
+		
+	}
 	
 	/**
 	 * Check if a player has 5 in row
@@ -53,7 +93,55 @@ public class GameGrid extends Observable{
 	 * @param player the player to check for
 	 * @return true if player has 5 in row, false otherwise
 	 */
-	public boolean isWinner(int player){}
-	
-	
+	public boolean isWinner(int player){
+		if(checkHorizontal(player) != true 
+				|| checkVertical(player) != true 
+				|| checkDiagonal(player) != true) {
+			return false;
+		} 
+		return true;
+		
+	}
+	private boolean checkHorizontal(int player) { //DENNA KOLLAR ÖVER GRÄNSERNA??
+		int rows = multiarray.length;
+		int cols = multiarray[0].length;
+		int count = 0;
+		while(count != INROW) {
+			for(int x = 0; x < rows; x++) {
+				for(int y = 0; y < cols; y++) {
+					if(multiarray[x][y] == player) {
+						count++;
+					} 
+					
+					if(multiarray[x][y] == EMPTY) {
+						count = 0;
+						
+					}
+				}
+			}
+		}
+		return true;
+	}
+	private boolean checkVertical(int player) {
+		int cols = multiarray[0].length;
+		int rows = multiarray.length;
+		int count = 0;
+		while(count != INROW) {
+			for(int y = 0; y < cols; y++) {
+				for(int x= 0; x < rows; x++) {
+					if(multiarray[x][y] == player) {
+						count++;
+					} 
+					if(multiarray[x][y] == EMPTY) {
+						count = 0;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	private boolean checkDiagonal(int player) {
+		
+	}
 }
+	
