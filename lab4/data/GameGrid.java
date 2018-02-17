81d9d3344655e548e53e09f857dd0754bebf78cf
Robin-Chendar <Robinbergstrm@gmail.com>
2018-02-17 23:52:48 +0100

    Lade till javadoc

--- lab4/data/GameGrid.java
+++ lab4/data/GameGrid.java
@@ -4,7 +4,10 @@ import java.util.Observable;
 
 
 /**
- * Represents the 2-d game grid
+ * The class GameGrid represents the board,
+ * a dynamic object of this class is the board
+ * you play on. 
+ *
  */
 
 public class GameGrid extends Observable{
@@ -16,9 +19,8 @@ public class GameGrid extends Observable{
 	public static final int INROW = 5;
 	
 	/**
-	 * Constructor
-	 * 
-	 * @param size The width/height of the game grid
+	 * Constructor that creates a board with the size
+	 * that is taken as a parameter for the dynamic object.
 	 */
 	
 	public GameGrid(int size){
@@ -27,40 +29,43 @@ public class GameGrid extends Observable{
 		startArray();
 	}
 	
+	/**
+	 * This method creates all the small boxes of the game,
+	 * the initial state of each box is EMPTY, which means
+	 * that no player controls that box.
+	 */
+	
 	private void startArray() {
 		for(int x = 0; x < multiarray.length; x++) {
 			for(int y = 0; y < multiarray.length; y++) {
 				multiarray[x][y] = EMPTY;
 			}
 		}
-	}	
+	}
+	
 	/**
-	 * Reads a location of the grid
-	 * 
-	 * @param x The x coordinate
-	 * @param y The y coordinate
-	 * @return the value of the specified location
+	 * This method returns the value of a specific box,
+	 * in other words it checks whether a box at position (x,y) 
+	 * is controlled by "me", "other" or if it's empty.
 	 */
 	public int getLocation(int x, int y){
 		return multiarray[x][y];
 	}
 		
 	/**
-	 * Returns the size of the grid
-	 * 
-	 * @return the grid size
+	 * This method checks the size of the grid.
 	 */
 	public int getSize(){
 		return this.size;
 	}
 	
 	/**
-	 * Enters a move in the game grid
-	 * 
-	 * @param x the x position
-	 * @param y the y position
-	 * @param player
-	 * @return true if the insertion worked, false otherwise
+	 * This method attempts making a move to a certain
+	 * box on the board. If no player has control over
+	 * that specific box the move will happen. However if
+	 * the box is not empty the move will not be made, instead
+	 * the method just returns false to say that the move was not
+	 * possible.
 	 */
 	public boolean move(int x, int y, int player){
 		if(multiarray[x][y] == EMPTY) {
@@ -75,7 +80,8 @@ public class GameGrid extends Observable{
 	}
 		
 	/**
-	 * Clears the grid of pieces
+	 * This method calls the function startArray() which creates
+	 * the board with only empty boxes, which means that it clears the board.
 	 */
 	public void clearGrid(){
 		startArray();
@@ -85,15 +91,17 @@ public class GameGrid extends Observable{
 	}
 	
 	/**
-	 * Check if a player has 5 in row
-	 * 
-	 * @param player the player to check for
-	 * @return true if player has 5 in row, false otherwise
+	 * This method returns true if any of the winning conditions are met,
+	 * the winning conditions are checked in help functions.
 	 */
 	public boolean isWinner(int player){
 		return checkHorizontal(player) || checkVertical(player) || checkDiagonal(player);
 		
 	}
+	/**
+	 * This method is a private help function that checks if
+	 * the player has enough boxes horizontally in a row to win the game.
+	 */
 	private boolean checkHorizontal(int player) { 
 		int rows = multiarray.length;
 		int cols = multiarray[0].length;
@@ -113,6 +121,10 @@ public class GameGrid extends Observable{
 		}
 		return false;
 	}
+	/**
+	 * This method is a private help function that checks if
+	 * the player has enough boxes vertically in a row to win the game.
+	 */
 	private boolean checkVertical(int player) {
 		int cols = multiarray[0].length;
 		int rows = multiarray.length;
@@ -132,6 +144,10 @@ public class GameGrid extends Observable{
 		}
 		return false;
 	}
+	/**
+	 * This method is a private help function that checks if
+	 * the player has enough boxes diagonally in a row to win the game.
+	 */
 	private boolean checkDiagonal(int player) { 
 		int cols = multiarray[0].length;
 		int rows = multiarray.length;
